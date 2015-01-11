4c7b719f2837e63b6867f2f447d27c0236384462
btabanpour <btabanpour@gmail.com>
2015-01-11 01:39:51 -0800

    asdf

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,7 @@
+var chart;
+var chartData = [];
+var day;
+var firstDate;
 $(document).ready(function () {
     // alert("message");
     $('#progress-div').css('display','inline');
@@ -82,10 +86,221 @@ $(document).ready(function () {
         $('#mileage').html(data.totalMi+' miles');
       }
     });
-
+    chartInit();
+    // dashboard_functions();
 
 
     $('#progress-div').css('display','none');
     $('.dashboard').css('display','');
-    $('#content').css('display', '');
+    $('.content').css('display', '');
 })
+
+function chartInit() {
+    
+    var chartCursor;
+    day = 0;
+    firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 50);
+
+    // generate some random data, quite different range
+    function generateChartData() {
+        for (day = 0; day < 30; day++) {
+            var newDate = new Date(firstDate);
+            newDate.setDate(newDate.getDate() + day);
+
+            var visits = Math.round(Math.random() * 50) - 6;
+
+            chartData.push({
+                date: newDate,
+                visits: visits
+            });
+        }
+    }
+
+    // create chart
+    AmCharts.ready(function() {
+      generateChartData();
+
+      // SERIAL CHART    
+      chart = new AmCharts.AmSerialChart();
+      chart.pathToImages = "http://www.amcharts.com/lib/images/";
+      chart.marginTop = 0;
+      chart.marginRight = 10;
+      chart.autoMarginOffset = 5;
+      chart.zoomOutButton = {
+          backgroundColor: '#000000',
+          backgroundAlpha: 0.15
+      };
+      chart.dataProvider = chartData;
+      chart.categoryField = "date";
+
+      // AXES
+      // category
+      var categoryAxis = chart.categoryAxis;
+      categoryAxis.parseDates = true; // as our data is date-based, we set parseDates to true
+      categoryAxis.minPeriod = "DD"; // our data is daily, so we set minPeriod to DD
+      categoryAxis.dashLength = 1;
+      categoryAxis.gridAlpha = 0.15;
+      categoryAxis.axisColor = "#DADADA";
+
+      // value                
+      var valueAxis = new AmCharts.ValueAxis();
+      valueAxis.axisAlpha = 0.2;
+      valueAxis.dashLength = 1;
+      chart.addValueAxis(valueAxis);
+
+      // GRAPH
+      var graph = new AmCharts.AmGraph();
+      graph.title = "red line";
+      graph.valueField = "visits";
+      graph.bullet = "round";
+      graph.bulletBorderColor = "#FFFFFF";
+      graph.bulletBorderThickness = 2;
+      graph.lineThickness = 2;
+      graph.lineColor = "#0352b5";
+      graph.negativeLineColor = "#b5030d";
+      graph.hideBulletsCount = 50; // this makes the chart to hide bullets when there are more than 50 series in selection
+      chart.addGraph(graph);
+
+      // CURSOR
+      chartCursor = new AmCharts.ChartCursor();
+      chartCursor.cursorPosition = "mouse";
+      chart.addChartCursor(chartCursor);
+
+      // SCROLLBAR
+      var chartScrollbar = new AmCharts.ChartScrollbar();
+      chartScrollbar.graph = graph;
+      chartScrollbar.scrollbarHeight = 40;
+      chartScrollbar.color = "#FFFFFF";
+      chartScrollbar.autoGridCount = false;
+      chart.addChartScrollbar(chartScrollbar);
+
+      // WRITE
+      chart.write("costChart");
+  })
+}
+
+
+function dashboard_functions() {
+  // aScore(.81, .85);
+  // costChart();
+
+}
+
+function costChart() {
+  // remove datapoint from the beginning
+  chart.dataProvider.shift();
+  
+  // add new one at the end
+  day++;
+  var newDate = new Date(firstDate);
+  newDate.setDate(newDate.getDate() + day);
+  var visits = Math.round(Math.random() * 100) - 40;
+  chart.dataProvider.push({
+      date: newDate,
+      visits: visits
+  });
+  chart.validateData();
+
+
+
+}
+
+function aScore(startPercent, endPercent) {
+  var colors = {
+    'pink': '#E1499A',
+    'yellow': '#f0ff08',
+    'green': '#47e495'
+  };
+
+  var color = colors.pink;
+
+  var radius = 100;
+  var border = 5;
+  var padding = 30;
+  // var startPercent = 0;
+  // var endPercent = 0.85;
+
+
+  var twoPi = Math.PI * 2;
+  var formatPercent = d3.format('.0%');
+  var boxSize = (radius + padding) * 2;
+
+
+  var count = Math.abs((endPercent - startPercent) / 0.01);
+  var step = endPercent < startPercent ? -0.01 : 0.01;
+
+  var arc = d3.svg.arc()
+      .startAngle(0)
+      .innerRadius(radius)
+      .outerRadius(radius - border);
+
+  var parent = d3.select('div#aScore');
+
+  var svg = parent.append('svg')
+      .attr('width', boxSize)
+      .attr('height', boxSize);
+
+  var defs = svg.append('defs');
+
+  var filter = defs.append('filter')
+      .attr('id', 'blur');
+
+
+  var g = svg.append('g')
+      .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
+
+  var meter = g.append('g')
+      .attr('class', 'progress-meter');
+
+  meter.append('path')
+      .attr('class', 'background')
+      .attr('fill', '#ccc')
+      .attr('fill-opacity', 0.5)
+      .attr('d', arc.endAngle(twoPi));
+
+  var foreground = meter.append('path')
+      .attr('class', 'foreground')
+      .attr('fill', color)
+      .attr('fill-opacity', 1)
+      .attr('stroke', color)
+      .attr('stroke-width', 5)
+      .attr('stroke-opacity', 1)
+      .attr('filter', 'url(#blur)');
+
+  var front = meter.append('path')
+      .attr('class', 'foreground')
+      .attr('fill', color)
+      .attr('fill-opacity', 1);
+
+  var numberText = meter.append('text')
+      .attr('fill', '#111')
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.35em');
+
+
+  function updateProgress(progress) {
+      foreground.attr('d', arc.endAngle(twoPi * progress));
+      front.attr('d', arc.endAngle(twoPi * progress));
+      numberText.html(formatPercent(progress)+'   Good');
+  }
+
+  var progress = startPercent;
+
+  (function loops() {
+      updateProgress(progress);
+
+      if (count > 0) {
+          count--;
+          progress += step;
+          setTimeout(loops, 10);
+      }
+  })();
+}
+
+
+
+
+
+
+
