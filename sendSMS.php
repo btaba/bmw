9a3a55fbc1054c718918f647bc2e3b4c043ae365
Tushar <imenzene@gmail.com>
2015-01-11 08:59:24 -0800

    sasd

--- sendSMS.php
+++ sendSMS.php
@@ -1,6 +1,6 @@
 <?php
 // Get the PHP helper library from twilio.com/docs/php/install
-require_once('/home/imbenzene/Documents/ami/php/twilio-php/Services/Twilio.php'); // Loads the library
+require_once('twilio-php/Services/Twilio.php'); // Loads the library
  
 // Your Account Sid and Auth Token from twilio.com/user/account
 $sid = "AC76de558201b9927e3bdb03f209f15a81"; 
